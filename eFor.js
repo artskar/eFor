e2f080a3c4b768923e3849ca593c5c8004c697b4
Skar <skarreg@gmail.com>
2017-01-24 15:19:09 +0800

    Create eFor.js

---
+++ eFor.js
@@ -0,0 +1,28 @@
+function eForStep(arr, arrLen, i, callback) {
+    if (i < arrLen) {
+        let item = arr[i];
+        callback(item);
+        setTimeout(eForStep.bind(this, arr, arrLen, i + 1, callback));
+    }
+}
+/**
+ * For statement emulation to work with large arrays without event loop blocking.
+ *
+ * eFor(arr, function (item) {
+ *    // do something with item
+ * });
+ *
+ *
+ * @param {Array} arr
+ * @param {Function} callback
+ */
+function eFor(arr, callback) {
+    let arrLen = arr.length;
+	if (arrLen < 2) {
+		callback(arr[0]);
+		return;
+	}
+	eForStep(arr, arrLen, 0, callback);
+}
+
+export {eFor, eForStep};
